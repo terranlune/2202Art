e57d478233fa6e0356f226bac882262f41dfd3ab
John Fuetsch <john.fuetsch@gmail.com>
2012-07-30 11:36:12 -0700

    Initial commit

---
+++ Arduino/fastspi_test/fastspi_test.ino
@@ -0,0 +1,147 @@
+#define DEBUG_SPI
+
+#include <FastSPI_LED.h>
+
+#define NUM_LEDS 250
+#define LIGHT_BOARD_WIDTH 19
+#define LIGHT_BOARD_HEIGHT 13
+
+// Sometimes chipsets wire in a backwards sort of way
+struct CRGB { unsigned char r; unsigned char g; unsigned char b; };
+// struct CRGB { unsigned char r; unsigned char g; unsigned char b; };
+struct CRGB *leds;
+
+//#define PIN 4
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hello");
+  
+  FastSPI_LED.setLeds(NUM_LEDS);
+  //FastSPI_LED.setChipset(CFastSPI_LED::SPI_TM1809);
+  //FastSPI_LED.setChipset(CFastSPI_LED::SPI_LPD6803);
+  //FastSPI_LED.setChipset(CFastSPI_LED::SPI_HL1606);
+  //FastSPI_LED.setChipset(CFastSPI_LED::SPI_595);
+  FastSPI_LED.setChipset(CFastSPI_LED::SPI_WS2801);
+
+  //FastSPI_LED.setPin(PIN); // Irrelevant for ws2801
+  
+  FastSPI_LED.setDataRate(3);
+  
+  FastSPI_LED.init();
+  FastSPI_LED.start();
+
+  leds = (struct CRGB*)FastSPI_LED.getRGBData(); 
+}
+
+void setLightBoardPixelColor(int x, int y, int r, int g, int b) {
+  // The strand starts at the bottom right and snakes up and to the 
+  // left, like so (assuming width of 4):
+  //
+  //  ^  11---10---09---08
+  //  |  04---05---06---07
+  //  y  03---02---01---00
+  //   x --->
+  // 
+  // So, y*width will tell us the minimum index it could be.  Since
+  // the rows alternate ordering, if the row is even, the additional
+  // offset is width-x-1.
+  int index = y * LIGHT_BOARD_WIDTH;
+  if (y%2==1) {
+      index += x;
+  }else{
+      index += (LIGHT_BOARD_WIDTH-x-1);
+  }
+  leds[index].r = r;
+  leds[index].g = g;
+  leds[index].b = b;
+}
+
+void setColor(int r, int g, int b) {
+  memset(leds, 0, NUM_LEDS * 3);
+  for (int i=0; i < NUM_LEDS; i++) {
+    leds[i].r = r;
+    leds[i].g = g;
+    leds[i].b = b;
+  }  
+}
+
+void showSinWave() {
+  for (float t=0;t<12.0*3.1415;t=t+.04) {
+    setColor( 8, 32, 16 );
+    for (int x=0; x<LIGHT_BOARD_WIDTH; x++) {
+      int y = int((sin(2.0*3.1415*x/LIGHT_BOARD_WIDTH+t)*0.8/2.0+0.5)*LIGHT_BOARD_HEIGHT);
+      setLightBoardPixelColor( x, y, 255, 64, 255 );
+    }
+    FastSPI_LED.show();
+  }
+}
+
+void loop() { 
+  // one at a time
+  for(int j = 0; j < 3; j++) { 
+    for(int i = 0 ; i < NUM_LEDS; i++ ) {
+      memset(leds, 0, NUM_LEDS * 3);
+      switch(j) { 
+        case 0: leds[i].r = 255; break;
+        case 1: leds[i].g = 255; break;
+        case 2: leds[i].b = 255; break;
+      }
+      FastSPI_LED.show();
+      delay(1);
+    }
+  }
+  
+  showSinWave();
+
+  // growing/receeding bars
+  for(int j = 0; j < 3; j++) { 
+    memset(leds, 0, NUM_LEDS * 3);
+    for(int i = 0 ; i < NUM_LEDS; i++ ) {
+      switch(j) { 
+        case 0: leds[i].r = 255; break;
+        case 1: leds[i].g = 255; break;
+        case 2: leds[i].b = 255; break;
+      }
+      FastSPI_LED.show();
+      delay(10);
+    }
+    for(int i = NUM_LEDS-1 ; i >= 0; i-- ) {
+      switch(j) { 
+        case 0: leds[i].r = 0; break;
+        case 1: leds[i].g = 0; break;
+        case 2: leds[i].b = 0; break;
+      }
+      FastSPI_LED.show();
+      delay(1);
+    }
+  }
+  
+  // Fade in/fade out
+  for(int j = 0; j < 3; j++ ) { 
+    memset(leds, 0, NUM_LEDS * 3);
+    for(int k = 0; k < 256; k++) { 
+      for(int i = 0; i < NUM_LEDS; i++ ) {
+        switch(j) { 
+          case 0: leds[i].r = k; break;
+          case 1: leds[i].g = k; break;
+          case 2: leds[i].b = k; break;
+        }
+      }
+      FastSPI_LED.show();
+      delay(3);
+    }
+    for(int k = 255; k >= 0; k--) { 
+      for(int i = 0; i < NUM_LEDS; i++ ) {
+        switch(j) { 
+          case 0: leds[i].r = k; break;
+          case 1: leds[i].g = k; break;
+          case 2: leds[i].b = k; break;
+        }
+      }
+      FastSPI_LED.show();
+      delay(3);
+    }
+  }
+}
---
+++ Arduino/jfuetsch/jfuetsch.ino
@@ -0,0 +1,261 @@
+#include "SPI.h"
+#include "Adafruit_WS2801.h"
+
+/*****************************************************************************
+Example sketch for driving Adafruit WS2801 pixels!
+
+
+  Designed specifically to work with the Adafruit RGB Pixels!
+  12mm Bullet shape ----> https://www.adafruit.com/products/322
+  12mm Flat shape   ----> https://www.adafruit.com/products/738
+  36mm Square shape ----> https://www.adafruit.com/products/683
+
+  These pixels use SPI to transmit the color data, and have built in
+  high speed PWM drivers for 24 bit color per pixel
+  2 pins are required to interface
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+
+*****************************************************************************/
+
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+// The colors of the wires may be totally different so
+// BE SURE TO CHECK YOUR PIXELS TO SEE WHICH WIRES TO USE!
+int dataPin  = 11;    // Yellow wire on Adafruit Pixels
+int clockPin = 13;    // Green wire on Adafruit Pixels
+
+// Don't forget to connect the ground wire to Arduino ground,
+// and the +5V wire to a +5V supply
+
+// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
+Adafruit_WS2801 strip = Adafruit_WS2801(250, dataPin, clockPin);
+
+// Optional: leave off pin numbers to use hardware SPI
+// (pinout is then specific to each board and can't be changed)
+//Adafruit_WS2801 strip = Adafruit_WS2801(25);
+
+// For 36mm LED pixels: these pixels internally represent color in a
+// different format.  Either of the above constructors can accept an
+// optional extra parameter: WS2801_RGB is 'conventional' RGB order
+// WS2801_GRB is the GRB order required by the 36mm pixels.  Other
+// than this parameter, your code does not need to do anything different;
+// the library will handle the format change.  Examples:
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin, WS2801_GRB);
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
+
+int lightBoardWidth = 19;
+int lightBoardHeight = 13;
+
+void setLightBoardPixelColor(int x, int y, uint32_t color) {
+  // The strand starts at the bottom right and snakes up and to the 
+  // left, like so (assuming width of 4):
+  //
+  //  ^  11---10---09---08
+  //  |  04---05---06---07
+  //  y  03---02---01---00
+  //   x --->
+  // 
+  // So, y*width will tell us the minimum index it could be.  Since
+  // the rows alternate ordering, if the row is even, the additional
+  // offset is width-x-1.
+  int index = y * lightBoardWidth;
+  if (y%2==1) {
+      index += x;
+  }else{
+      index += (lightBoardWidth-x-1);
+  }
+  strip.setPixelColor(index, color);
+}
+
+
+void setup() {
+    
+  strip.begin();
+
+  // Update LED contents, to start they are all 'off'
+  strip.show();
+}
+
+
+void loop() {
+  // Some example procedures showing how to display to the pixels
+  showSinWave( Color( 255, 64, 255 ) );
+  colorWipe(Color(255, 0, 0), 0);
+  colorWipe(Color(0, 255, 0), 50);
+  colorWipe(Color(0, 0, 255), 50);
+  rainbow(20);
+  rainbowCycle(20);
+//  showColor( Color(255,255,255) );
+
+//    showChecker( Color(255,255,255), Color(0,0,0) );
+//    showColor( HSVColor(0.5, 0.75, 1.0) );
+}
+
+void showSinWave(uint32_t color) {
+  for (float t=0;t<12.0*3.1415;t=t+.04) {
+    setColor( Color( 8, 32, 16) );
+    for (int x=0; x<lightBoardWidth; x++) {
+      int y = int((sin(2.0*3.1415*x/lightBoardWidth+t)*0.8/2.0+0.5)*lightBoardHeight);
+      setLightBoardPixelColor( x, y, color );
+    }
+    strip.show();
+  }
+}
+
+void showChecker(uint32_t color1, uint32_t color2) {
+  int i;
+    for (i=0; i < strip.numPixels(); i++) {
+      if(i%2==0){
+        strip.setPixelColor(i, color1);
+      }else{
+        strip.setPixelColor(i, color2);
+      }
+        
+    }  
+    strip.show();   // write all the pixels out
+}
+
+void setColor(uint32_t color) {
+  for (int i=0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+      
+  }  
+}
+
+void showColor(uint32_t color) {
+  setColor(color);
+    strip.show();   // write all the pixels out
+}
+
+void rainbow(uint8_t wait) {
+  int i, j;
+   
+  for (j=0; j < 256; j++) {     // 3 cycles of all 256 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel( (i + j) % 255));
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  
+  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// fill the dots one after the other with said color
+// good for testing purposes
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+  
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+/* Helper functions */
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+uint32_t HSVColor(float h, float s, float v)
+{
+  float r, g, b;
+  HSVtoRGB(&r, &g, &b, h, s, v);
+  return Color(r*255, g*255, b*255); 
+}
+
+void HSVtoRGB( float *r, float *g, float *b, float h, float s, float v )
+{
+	int i;
+	float f, p, q, t;
+	if( s == 0 ) {
+		// achromatic (grey)
+		*r = *g = *b = v;
+		return;
+	}
+	h /= 60;			// sector 0 to 5
+	i = floor( h );
+	f = h - i;			// factorial part of h
+	p = v * ( 1 - s );
+	q = v * ( 1 - s * f );
+	t = v * ( 1 - s * ( 1 - f ) );
+	switch( i ) {
+		case 0:
+			*r = v;
+			*g = t;
+			*b = p;
+			break;
+		case 1:
+			*r = q;
+			*g = v;
+			*b = p;
+			break;
+		case 2:
+			*r = p;
+			*g = v;
+			*b = t;
+			break;
+		case 3:
+			*r = p;
+			*g = q;
+			*b = v;
+			break;
+		case 4:
+			*r = t;
+			*g = p;
+			*b = v;
+			break;
+		default:		// case 5:
+			*r = v;
+			*g = p;
+			*b = q;
+			break;
+	}
+}
