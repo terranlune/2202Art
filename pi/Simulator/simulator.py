45eb3987b4300bc8a0f81c5aa31e40f09fc98c21
BurlapRobot <burlaprobot@gmail.com>
2012-12-12 14:20:02 -0800

    Added importer logic for more integration

--- pi/Simulator/simulator.py
+++ pi/Simulator/simulator.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python
 
 from PyQt4 import QtGui, QtCore
-import sys 
-
+import sys
+import painter
 SIZE = 20
 
+        
 class Example(QtGui.QWidget):
     Size = 20
     Speed = 100
 
-    def __init__(self):
+    def __init__(self, painters):
         super(Example, self).__init__()
-        self.pixelBuffer = []
-        pixel = 0
-        for x in range(19):
-            for y in range(13):
-                self.pixelBuffer.append((pixel, pixel, pixel))
-                pixel +=1
-        
-        self.timer = QtCore.QBasicTimer()
+        self.painters = painters
+        for painter in self.painters:
+           painter.setup()
+        self.painterIndex = 0
+        self.tick = 0
+        self.pixelBuffer = self.painters[self.painterIndex].pixelBuffer
+        self.timer = QtCore.QTimer()
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.timerUpdate)
         self.initUI()
 
-    def initUI(self):      
-        
+
+    def initUI(self):
         self.setGeometry(300, 300, 19*SIZE, 13*SIZE)
         self.setWindowTitle('MA Sim')
+        self.timer.start(10)
+        self.painters[self.painterIndex].draw()
         self.show()
 
+    def timerUpdate(self):
+        self.repaint()
+        self.painters[self.painterIndex].draw()
+        self.timer.start(10)
+
     def paintEvent(self, event):
-        print self.pixelBuffer
         qp = QtGui.QPainter()
         qp.begin(self)
         self.drawFrame(event, qp)
@@ -40,9 +47,9 @@ class Example(QtGui.QWidget):
                 self.drawPixel(qp, x, y)
 
     def drawPixel(self, qp, x, y):
-        colorT = self.pixelBuffer[y*19+x]
+        colorT = self.pixelBuffer.getPixel(x,y)
         qp.setBrush(QtGui.QColor(colorT[0], colorT[1], 
-                                 colorT[2]))
+                                             colorT[2]))
         qp.drawRect(x*SIZE, y*SIZE, SIZE, SIZE)
 
 
@@ -52,9 +59,11 @@ class Example(QtGui.QWidget):
         else:
             QtGui.QFrame.timerEvent(self, event)
         
-def main():    
+def main():        
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    mod = __import__(sys.argv[1])
+    cls = getattr(mod, "UserPainter")
+    ex = Example([cls()])
     sys.exit(app.exec_())
 
 
