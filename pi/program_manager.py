e177306e86b7e83815be4b643f9fc4c0e615fc28
BurlapRobot <burlaprobot@gmail.com>
2012-12-12 16:25:47 -0800

    fixed indents

--- pi/program_manager.py
+++ pi/program_manager.py
@@ -17,41 +17,42 @@ spiBytes = bytearray(numPixels * 3 + 1)
 
 # For testing, solid red
 #for pixel in range(numPixels):
-#	spiBytes[pixel*3 + 0] = 0
-#	spiBytes[pixel*3 + 1] = 255
-#	spiBytes[pixel*3 + 2] = 0
+#       spiBytes[pixel*3 + 0] = 0
+#       spiBytes[pixel*3 + 1] = 255
+#       spiBytes[pixel*3 + 2] = 0
+
 
 
 def getIndex(x, y):
-	"""
-	The strand starts at the bottom right and snakes up and to the 
-	left, like so (assuming width of 4):
+    """
+    The strand starts at the bottom right and snakes up and to the 
+    left, like so (assuming width of 4):
 
-	  ^  11---10---09---08
-	  |  04---05---06---07
-	  y  03---02---01---00
-	   x --->
-	 
-	 So, y*width will tell us the minimum index it could be.  Since
-	 the rows alternate ordering, if the row is even, the additional
-	 offset is width-x-1.
-	"""
-	index = y * boardWidth + 1;
-	if (y%2==1):
-	  index += x
-	else:
-	  index += (boardWidth-x-1)
+      ^  11---10---09---08
+      |  04---05---06---07
+      y  03---02---01---00
+       x --->
 
-	return index
+     So, y*width will tell us the minimum index it could be.  Since
+     the rows alternate ordering, if the row is even, the additional
+     offset is width-x-1.
+    """
+    index = y * boardWidth + 1;
+    if (y%2==1):
+      index += x
+    else:
+      index += (boardWidth-x-1)
+
+    return index
 
 def setIndexColor(i, r, g, b):
-	spiBytes[i*3 + 0] = r
-	spiBytes[i*3 + 1] = g
-	spiBytes[i*3 + 2] = b
+    spiBytes[i*3 + 0] = r
+    spiBytes[i*3 + 1] = g
+    spiBytes[i*3 + 2] = b
 
 def setPixelColor(x, y, r, g, b):
-	i = getIndex(x, y)
-	setIndexColor(i, r, g, b)
+    i = getIndex(x, y)
+    setIndexColor(i, r, g, b)
 
 
 def main():
@@ -61,15 +62,15 @@ def main():
     painter.setup()
     while(1):
         painter.draw()
-	    for x in range(boardWidth):
-		    for y in range(boardHeight):
+        for x in range(boardWidth):
+            for y in range(boardHeight):
                 color = painter.pixelBuffer.getPixel(x,y)
-			    try:
-				    setPixelColor(x, y, color[0], color[1], color[2] )
-			    except IndexError:
-				    setPixelColor(x, y, 0, 0, 0)
+                try:
+                    setPixelColor(x, y, color[0], color[1], color[2] )
+                except IndexError:
+                    setPixelColor(x, y, 0, 0, 0)
 
-	    # Display the pixels
-	    spiBytes[numPixels] = 0 # Make sure latch is set
-	    spidev.write(spiBytes)
-	    spidev.flush()
+        # Display the pixels
+        spiBytes[numPixels] = 0 # Make sure latch is set
+        spidev.write(spiBytes)
+        spidev.flush()
